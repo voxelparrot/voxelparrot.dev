d1fe4ea53d09b6d8c4bd4c32b0da258f95ec2898
voxelparrot <voxelparrot@gmail.com>
2025-09-15 14:39:39 -0700

    bowleyes

--- client/src/components/LoadingComponent.tsx
+++ client/src/components/LoadingComponent.tsx
@@ -10,7 +10,7 @@ export default function LoadingComponent() {
               <div className="h-8 bg-muted rounded w-64 mx-auto mb-4"></div>
               <div className="h-4 bg-muted rounded w-96 mx-auto mb-4"></div>
               <div className="h-2 bg-muted rounded w-64 mx-auto mb-4"></div>
-              <p className="h-2 text-muted rounded w-64 mx-auto mb-4"> Cool Loading</p>
+              <p className="h-2 text-muted rounded w-64 mx-auto mb-4"> Bowl Eyes</p>
               <div className="h-4 bg-muted rounded w-64 mx-auto mb-4"></div>
               <p className="h-2 text-muted rounded w-64 mx-auto mb-4"> Secret</p>
             </div>
