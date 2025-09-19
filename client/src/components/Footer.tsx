c4b73e8116a93bb61dab26baee6b3b4a705a3237
voxelparrot <voxelparrot@gmail.com>
2025-09-18 21:17:35 -0700

    leon

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,18 +4,36 @@ export default function Footer() {
       className="py-8 px-4 sm:px-6 lg:px-8 border-t border-border"
       data-testid="footer"
     >
-      <div className="max-w-7xl mx-auto text-center">
-        <p
-          className="text-muted-foreground text-xs mt-2"
-          data-testid="footer-tagline"
-        >
-          yo whats up
-        </p>
+    <div className="flex items-center justify-center gap-4">
+      <div className="max-w-7xl mx-auto text-center flex items-center justify-center">
+        <a
+                          href="https://voxelparrot.dev"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="cursor-pointer"
+                          data-testid="link-parrot"
+                        >
         <img
           src={"/assets/parrotchair.png"}
           alt={"Parrot Chair"}
           className="w-10 h-9 object-cover mx-auto"
         />
+        </a>
+        <div className="p-4"></div>
+        <a
+                          href="https://leoniscool.com"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="cursor-pointer"
+                          data-testid="link-leon"
+                        >
+        <img
+          src={"/assets/leon.png"}
+          alt={"Leon"}
+          className="w-9 h-9 object-cover mx-auto"
+        />
+        </a>
+      </div>
       </div>
     </footer>
   );
